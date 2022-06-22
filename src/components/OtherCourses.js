0bacd955ee965aa9be487b200ad83667b51eb792
Dvid3r <Dvid3r@gmail.com>
2022-06-22 02:51:39 -0700

    aligned 'share this training' header to the left

--- src/components/OtherCourses.js
+++ src/components/OtherCourses.js
@@ -10,17 +10,17 @@ export default function OtherCourses() {
       <section>
         <div className="container">
           <SimilarSlider />
-          <div className="container">
-            <div className="share-area">
-              <h3 style={{ marginTop: "10px", borderBottom: "1px solid", color:"#333333" }}>Share this training</h3>
-            </div>
-            <div className="social-links" style={{marginTop: "-10px", color:"#333333" }}>
-              {/* <a href="/" className='link-item' target="_blank" rel="noreferrer"><FaLinkedin /></a> */}
-              <a href="https://www.facebook.com/100173285999843/posts/129509603066211" className='link-item' target="_blank" rel="noreferrer"><FaFacebookSquare /></a>
-              <a href="https://www.instagram.com/empowerwit2022/?igshid=YmMyMTA2M2Y" className='link-item' target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
-              <a href="https://twitter.com/EWIT_Official?t=KGqw1IpsFT9DIGxgytg6qg&s=09" className='link-item' target="_blank" rel="noreferrer"><FaTwitterSquare /></a>
-              <a href="https://discord.com/invite/WwfnzEHx" className='link-item' target="_blank" rel="noreferrer"><BsDiscord /></a>
-            </div>
+        </div>
+        <div className="container">
+          <div className="share-area">
+            <h3 style={{ marginTop: "10px", borderBottom: "1px solid", color:"#333333" }}>Share this training</h3>
+          </div>
+          <div className="social-links" style={{marginTop: "-10px", color:"#333333" }}>
+            {/* <a href="/" className='link-item' target="_blank" rel="noreferrer"><FaLinkedin /></a> */}
+            <a href="https://www.facebook.com/100173285999843/posts/129509603066211" className='link-item' target="_blank" rel="noreferrer"><FaFacebookSquare /></a>
+            <a href="https://www.instagram.com/empowerwit2022/?igshid=YmMyMTA2M2Y" className='link-item' target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
+            <a href="https://twitter.com/EWIT_Official?t=KGqw1IpsFT9DIGxgytg6qg&s=09" className='link-item' target="_blank" rel="noreferrer"><FaTwitterSquare /></a>
+            <a href="https://discord.com/invite/WwfnzEHx" className='link-item' target="_blank" rel="noreferrer"><BsDiscord /></a>
           </div>
         </div>
       </section>
